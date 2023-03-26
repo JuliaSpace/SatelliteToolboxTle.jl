e228c3166d69a756b07acfaf1f3ce07edcd24323
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-03-25 21:11:14 -0300

    :rotating_light: Improve test coverage

--- test/parse_tles_from_strings.jl
+++ test/parse_tles_from_strings.jl
@@ -87,6 +87,28 @@ end
         :error,
         "Wrong checksum in TLE line 2 (expected = 2, found = 5)."
     ) try read_tle(l1, l2) catch end
+
+    # Invalid lines
+    # ==========================================================================
+
+    l1 = "       2 47699U 21015A   23083.68657856 -.00000044  10000-8  43000-4 0  9990"
+    l2 = " 2 47699  98.4304 162.1097 0001247 136.2017 223.9283 14.40814394108655"
+
+    @test_throws ArgumentError read_tle(l1, l2)
+    @test_logs (
+        :error,
+        "The 1st line is not valid."
+    ) try read_tle(l1, l2) catch end
+
+    l1 = "       1 47699U 21015A   23083.68657856 -.00000044  10000-8  43000-4 0  9990"
+    l2 = " 3 47699  98.4304 162.1097 0001247 136.2017 223.9283 14.40814394108652"
+
+    @test_throws ArgumentError read_tle(l1, l2)
+    @test_logs (
+        :error,
+        "The 2nd line is not valid."
+    ) try read_tle(l1, l2) catch end
+
 end
 
 @testset "Function read_tle (string)" begin
@@ -265,6 +287,37 @@ end
         """
 
     @test_throws ArgumentError read_tle(tle_str)
+
+    # Invalid lines
+    # ==========================================================================
+
+    tle_str = """
+        # This line should be ignored
+             AMAZONIA 1
+          2 47699U 21015A   23083.68657856 -.00000044  10000-8  43000-4 0  9990
+        # This is a comment.
+                2 47699  98.4304 162.1097 0001247 136.2017 223.9283 14.40814394108652
+        """
+
+    @test_throws ArgumentError read_tle(tle_str)
+    @test_logs (
+        :error,
+        "The 1st line is not valid."
+    ) try read_tle(tle_str) catch end
+
+    tle_str = """
+        # This line should be ignored
+             AMAZONIA 1
+          1 47699U 21015A   23083.68657856 -.00000044  10000-8  43000-4 0  9990
+        # This is a comment.
+                3 47699  98.4304 162.1097 0001247 136.2017 223.9283 14.40814394108652
+        """
+
+    @test_throws ArgumentError read_tle(tle_str)
+    @test_logs (
+        :error,
+        "The 2nd line is not valid."
+    ) try read_tle(tle_str) catch end
 end
 
 # Function: read_tles
@@ -373,6 +426,63 @@ end
     cbers_tle = tles |> last
 
     @test cbers_tle.name                     == "CBERS 4A"
+    @test cbers_tle.satellite_number         == 44883
+    @test cbers_tle.classification           == 'U'
+    @test cbers_tle.international_designator == "19093E"
+    @test cbers_tle.epoch_year               == 23
+    @test cbers_tle.epoch_day                == 84.50188177
+    @test cbers_tle.dn_o2                    == 4.132e-5
+    @test cbers_tle.ddn_o6                   == 0.0
+    @test cbers_tle.bstar                    ≈  0.53225e-3
+    @test cbers_tle.element_set_number       == 999
+    @test cbers_tle.inclination              == 97.8666
+    @test cbers_tle.raan                     == 164.4776
+    @test cbers_tle.eccentricity             == 0.0001781
+    @test cbers_tle.argument_of_perigee      == 94.0485
+    @test cbers_tle.mean_anomaly             == 266.0964
+    @test cbers_tle.mean_motion              == 14.81596492
+    @test cbers_tle.revolution_number        == 17640
+
+    # No satellite name
+    # ==========================================================================
+
+    tles_str = """
+        # This line should be ignored
+          1 47699U 21015A   23083.68657856 -.00000044  10000-8  43000-4 0  9990
+        # This is a comment.
+            2 47699  98.4304 162.1097 0001247 136.2017 223.9283 14.40814394108659
+            # Another comment
+            1 44883U 19093E   23084.50188177  .00004132  00000+0  53225-3 0  9999
+            2 44883  97.8666 164.4776 0001781  94.0485 266.0964 14.81596492176409
+        """
+
+    tles = read_tles(tles_str; verify_checksum = false)
+
+    @test length(tles) == 2
+
+    amz1_tle = tles |> first
+
+    @test amz1_tle.name                     == "UNDEFINED"
+    @test amz1_tle.satellite_number         == 47699
+    @test amz1_tle.classification           == 'U'
+    @test amz1_tle.international_designator == "21015A"
+    @test amz1_tle.epoch_year               == 23
+    @test amz1_tle.epoch_day                == 83.68657856
+    @test amz1_tle.dn_o2                    == -4.4e-7
+    @test amz1_tle.ddn_o6                   == 1.0e-9
+    @test amz1_tle.bstar                    == 4.3e-5
+    @test amz1_tle.element_set_number       == 999
+    @test amz1_tle.inclination              == 98.4304
+    @test amz1_tle.raan                     == 162.1097
+    @test amz1_tle.eccentricity             == 0.0001247
+    @test amz1_tle.argument_of_perigee      == 136.2017
+    @test amz1_tle.mean_anomaly             == 223.9283
+    @test amz1_tle.mean_motion              == 14.40814394
+    @test amz1_tle.revolution_number        == 10865
+
+    cbers_tle = tles |> last
+
+    @test cbers_tle.name                     == "UNDEFINED"
     @test cbers_tle.satellite_number         == 44883
     @test cbers_tle.classification           == 'U'
     @test cbers_tle.international_designator == "19093E"
@@ -805,6 +915,26 @@ end
     @test_logs (
         :error,
         "[Line 5]: The revolution number in the TLE line 2 could not be parsed."
+    ) read_tles(tles_str; verify_checksum = false)
+    @test length(read_tles(tles_str; verify_checksum = false)) == 1
+
+    # Incomplete TLEs
+    # ==========================================================================
+
+    tles_str = """
+        # This line should be ignored
+             AMAZONIA 1
+          1 47699U 21015A   23083.68657856 -.00000044  10000-8  43000-4 0  9990
+        # This is a comment.
+            2 47699  98.4304 162.1097 0001247 136.2017 223.9283 14.40814394108652
+            # Another comment
+        CBERS 4A
+            1 44883U 19093E   23084.50188177  .00004132  00000+0  53225-3 0  9992
+        """
+
+    @test_logs (
+        :error,
+        "[Line 8]: The last TLE in the file is incomplete."
     ) read_tles(tles_str; verify_checksum = false)
     @test length(read_tles(tles_str; verify_checksum = false)) == 1
 end
